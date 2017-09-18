12458cc71858915080788c48e166afc9abe76d1b
Charleston Telles <charlestontelles@gmail.com>
2017-09-18 16:40:36 +1000

    Update docker-entrypoint.sh
    hardcode password "admin"

--- GlassFish/4.1.2/docker-entrypoint.sh
+++ GlassFish/4.1.2/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # 
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
 if [[ -z $ADMIN_PASSWORD ]]; then
-	ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
+	ADMIN_PASSWORD=admin
 	echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
 fi
 echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
